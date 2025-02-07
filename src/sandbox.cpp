7f1f37ce586cfb834bd662fab0d6be89a4a2ffb7
Per Mathisen <per.mathisen@arm.com>
2025-02-07 17:51:44 +0100

    Fix compiler warning

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -33,17 +33,6 @@ landlock_create_ruleset(const struct landlock_ruleset_attr *const attr,
 }
 #endif
 
-#ifndef landlock_add_rule
-static inline int landlock_add_rule(const int ruleset_fd,
-				    const enum landlock_rule_type rule_type,
-				    const void *const rule_attr,
-				    const __u32 flags)
-{
-	return syscall(__NR_landlock_add_rule, ruleset_fd, rule_type, rule_attr,
-		       flags);
-}
-#endif
-
 #ifndef landlock_restrict_self
 static inline int landlock_restrict_self(const int ruleset_fd,
 					 const __u32 flags)
